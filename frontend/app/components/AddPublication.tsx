b87d6327ec4e4f53ceae6dbc0e271c6f3a9401fb
AMS <111883236+AMS003010@users.noreply.github.com>
2024-05-07 18:22:27 +0530

    added a few fixes

--- frontend/app/components/AddPublication.tsx
+++ frontend/app/components/AddPublication.tsx
@@ -26,109 +26,130 @@ export default function AddPublication () {
     return(
         <div className="bg-[#d5e7eb] h-dvh w-screen flex justify-end overflow-hidden overflow-y-auto">
             <div className="h-max bg-white m-3 w-screen md:w-[79%] rounded-2xl p-7 flex flex-col shadow">
-                <h1 className={`${roboto.className} text-black text-4xl lg:text-4xl mt-7`}>Add a Publication</h1>
-                <hr className="flex justify-center border-t-2 border-gray-200 mt-7 mb-20"/>
+                <h1 className={`${roboto.className} text-black text-4xl lg:text-4xl mt-12 lg:mt-7`}>Add a Publication</h1>
+                <hr className="flex justify-center border-t-2 border-red-300 mt-7 mb-20"/>
                 <form>
                     <div className='flex justify-between gap-5 mb-7'>
                         <div className='relative w-full'>
                             <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='Faculty Name'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>Name</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>Name</label>
                         </div>
                         <div className='relative w-full'>
-                            <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                            {/* <input 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='Type'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>Type</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>Type</label> */}
+                            <select name="capstone" id="capstone" className='text-gray-400 text-lg outline-none pb-3 pr-[10%] lg:pr-[71%] focus:text-rose-400'>
+                                <option value="type">Type</option>
+                                <option value="">Journal</option>
+                                <option value="published">Conference</option>
+                                <option value="published">Book Chapter</option>
+                            </select>
+                            <hr className="flex justify-center border-t-2 border-gray-300 peer-focus:text-rose-400"/>
                         </div>
                     </div>
                     <div className='flex justify-between gap-5 mb-7'>
                         <div className='relative w-full'>
                             <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='Title'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>Title</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>Title</label>
                         </div>
                     </div>
-                    <div className='flex justify-between gap-5 mb-7'>
+                    <div className='lg:flex justify-between gap-5 lg:mb-7'>
                         <div className='relative w-full'>
                             <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='Name of Journal / Book / Conference'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>Name of Journal / Book / Conference</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>Name of Journal / Book / Conference</label>
                         </div>
                         <div className='relative w-full'>
-                            <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                            {/* <input 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='Status'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>Status</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>Status</label> */}
+                            <select name="Status" id="status" className='text-gray-400 text-lg outline-none pb-3 pr-[77%] focus:text-rose-400'>
+                                <option value="status">Status</option>
+                                <option value="accepted">Accepted</option>
+                                <option value="published">Published</option>
+                                <option value="pending">Pending</option>
+                            </select>
+                            <hr className="flex justify-center border-t-2 border-gray-300 peer-focus:text-rose-400"/>
                         </div>
                     </div>
                     <div className='flex justify-between gap-5 mb-7'>
                         <div className='relative w-full'>
                             <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='No of Total Authors'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>No of Total Authors</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>No of Total Authors</label>
                         </div>
                         <div className='relative w-full'>
                             <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='Author Names'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>Author Names</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>Author Names</label>
                         </div>
                     </div>
                     <div className='flex justify-between gap-5 mb-7'>
                         <div className='relative w-full'>
-                            <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                            {/* <input 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='Capstone Project'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>Capstone Project ( Yes / No )</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>Capstone Project ( Yes / No )</label> */}
+                            <select name="capstone" id="capstone" className='text-gray-400 text-lg outline-none pb-3 pr-[48%] focus:text-rose-400'>
+                                <option value="status">Capstone Project ( Yes / No )</option>
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                            <hr className="flex justify-center border-t-2 border-gray-300 peer-focus:text-rose-400"/>
                         </div>
                         <div className='relative w-full'>
                             <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='Links'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>Links</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>Links</label>
                         </div>
                     </div>
                     <div className='flex justify-between gap-5 mb-7'>
                         <div className='relative w-full'>
                             <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='Impact Factor'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>Impact Factor</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>Impact Factor</label>
                         </div>
                         <div className='relative w-full'>
                             <input 
-                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600' 
+                                className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-400' 
                                 type='text'
                                 placeholder='Scopus Indexation'
                             />
-                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm'>Scopus Indexation</label>
+                            <label className='absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-rose-400 peer-focus:text-sm peer-focus:font-semibold'>Scopus Indexation</label>
                         </div>
                     </div>
+                    <button type='submit' className='w-full text-red-500 text-2xl font-bold border-red-500 border-solid border-2 p-3 rounded-lg flex items-center justify-center hover:bg-red-300 hover:text-white hover:border-white'>Submit</button>
                 </form>
             </div>
         </div>
