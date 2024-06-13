4405ee6dc8de7d2a75a9220b55cc64d36ed39fb0
AMS <111883236+AMS003010@users.noreply.github.com>
2024-06-13 19:21:25 +0530

    added the compare section

--- frontend/app/components/SidePanel.tsx
+++ frontend/app/components/SidePanel.tsx
@@ -62,7 +62,7 @@ export default function SidePanel({index,setIndex}: SidePanelProps) {
                             </svg>
                             <span className={`${nunito.className} text-lg ml-2`}>Add a Publication</span>
                         </div>
-                        <div className='flex justify-start items-center my-1 hover:bg-[#21205e] rounded-lg py-4 text-[#FFF] hover:text-white pl-3 cursor-pointer'>
+                        <div className='flex justify-start items-center my-1 hover:bg-[#21205e] rounded-lg py-4 text-[#FFF] hover:text-white pl-3 cursor-pointer' onClick={() => setIndex(2)}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
                             </svg>
