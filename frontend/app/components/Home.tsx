734ad60274cb26ef0ab59ff8a1d13e637bf69a0b
AMS <111883236+AMS003010@users.noreply.github.com>
2024-06-21 23:45:12 +0530

    added some ui changes

--- frontend/app/components/Home.tsx
+++ frontend/app/components/Home.tsx
@@ -215,7 +215,7 @@ export default function Home() {
 
   return (
     <main className="bg-[#d5e7eb] h-max w-screen flex justify-end overflow-y-auto">
-      <div className="h-max bg-white m-3 w-screen md:w-[79%] rounded-2xl p-7 flex flex-col shadow">
+      <div className="h-max bg-white m-3 w-[94%] md:w-[79%] rounded-2xl p-7 flex flex-col shadow">
         <div className='flex justify-between flex-row w-[100%]'>
           <div className='flex justify-start flex-col pt-8'>
             <h1 className={`${roboto.className} text-black text-4xl lg:text-4xl`}>Publications</h1>
@@ -237,8 +237,8 @@ export default function Home() {
         </div>
         <hr className="flex justify-center border-t-2 border-gray-200 mt-5 mb-2"/>
 
-        <div className='flex justify-between mt-9 mb-4'>
-          <div className='flex justify-start'>
+        <div className='flex justify-between items-center mt-9 mb-4'>
+          <div className='flex justify-start items-center'>
             <div>
               <input 
                 className='text-gray-900 border-2 border-gray-100 focus:outline-none focus:border-gray-400 p-2 rounded-xl shadow-md hover:cursor-pointer text-[0.7rem] lg:text-[1rem] mr-2 lg:mr-0'
@@ -266,13 +266,13 @@ export default function Home() {
                 onChange={(e) => setfilterEndDate(e.target.value)}
               />
             </div>
-            <div className='text-gray-500 border-gray-100 border-2 rounded-xl p-2 ml-2 hover:border-gray-400 hover:cursor-pointer shadow-md  text-[0.7rem] lg:text-[1rem]' onClick={() => handleDateFilter()}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <div className='text-gray-500 border-gray-100 border-2 rounded-xl p-2 flex items-center md:p-2 ml-2 hover:border-gray-400 hover:cursor-pointer shadow-md  text-[0.7rem] lg:text-[1rem]' onClick={() => handleDateFilter()}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 lg:w-6 lg:h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
               </svg>
             </div>
           </div>
-          <div className='text-gray-500 border-gray-100 border-2 rounded-xl p-2 ml-2 hover:border-gray-400 hover:cursor-pointer shadow-md  text-[0.7rem] lg:text-[1rem]'>
+          <div className='text-gray-500 border-gray-100 border-2 rounded-xl p-2 ml-2 hover:border-gray-400 hover:cursor-pointer shadow-md  text-[0.7rem] lg:text-[1rem] hidden md:block'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
             </svg>
